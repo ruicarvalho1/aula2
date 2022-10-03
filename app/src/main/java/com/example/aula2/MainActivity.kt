39b5d79ec983b0123156a8d33d261fe477ff0ea7
ruicarvalho1 <ruicarvalhor13@gmail.com>
2022-10-03 11:32:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aula2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.aula2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("TAG", "onstart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG", "onresume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TAG", "onpause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TAG", "ondestroy")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TAG", "onstop")
+    }
+}
